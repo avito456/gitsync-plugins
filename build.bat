b13bdd5b757c4617aeef61b73710046c92ce60cc
Gogs <gogs@fake.local>
2021-01-17 20:04:41 +0300

    Подготовлен начальный образ для публикации мобильной конфигурации на web

---
+++ build.bat
@@ -0,0 +1,14 @@
+echo Плагины компилятся в каталог %localappdata%\gitsync\plugins
+
+CALL opm run testing-build
+
+
+REM CALL gitsync plugins list -a
+
+CALL gitsync sync --help
+REM CALL C:\Workspace\builds\WL4CKQi4\0\ci-cd\push-1c-mobile-to-git\Sync_mob-tsd11.bat
+
+
+CALL C:\Workspace\builds\WL4CKQi4\0\ci-cd\push-1c-mobile-to-git\Sync_mob-tsd10.bat
+
+
